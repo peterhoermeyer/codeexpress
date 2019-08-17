6386111511a506d2cb0fd8de9f496128065cd17b
Peter Hörmeyer <peter@hoermeyer.net>
2019-08-18 00:49:39 +0200

    add nodes table

---
+++ database/migrations/2019_08_17_224711_add_nodes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddNodesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('node', function (Blueprint $table) {
+            $table->bigIncrements('id')->primary();
+            $table->string('title')->nullable();
+            $table->text('content')->nullable();
+            $table->bigInteger('parent_node_id')->nullable();
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}

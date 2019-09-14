28d272a9bb720361a5ea873b80321b5a057d71ea
Peter Hörmeyer <peter@hoermeyer.net>
2019-09-14 15:03:18 +0200

    added failing test

--- public/js/app.js
+++ public/js/app.js
@@ -37031,9 +37031,7 @@ var staticRenderFns = [
             ]),
             _vm._v(" "),
             _c("div", { staticClass: "card-body" }, [
-              _vm._v(
-                "\n                    I'm an example component.\n                "
-              )
+              _vm._v("\n                    Hiho! 23\n                ")
             ])
           ])
         ])
